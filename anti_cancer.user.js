7831566e40c4de5364b4d2c2eb469ec3041ed703
NocturneJVC <jedisbonjourbordel@gmail.com>
2017-10-11 22:18:08 +0200

    version 2.1

--- anti_cancer.user.js
+++ anti_cancer.user.js
@@ -1,20 +1,22 @@
 // ==UserScript==
 // @name         Traitement contre le cancer (anti topic "post ou...")
-// @version      2.0
+// @version      2.1
 // @description  N'attrapez plus le cancer !
 // @author       NocturneX
 // @match        http://www.jeuxvideo.com/forums/*
 // @match        http://www.jeuxvideo.com/recherche/forums/*
 // @grant        GM_xmlhttpRequest
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @icon         http://image.noelshack.com/fichiers/2016/36/1473604760-picsart-09-03-11-39-59.jpg
 // ==/UserScript==
 
-// MÀJ 2.0: prend maintenant en compte le sujet (exemple: sujet "cette meuf à la post", message "ou cancer :)" sera détecté)
+// MÀJ 2.1: le script se souvient maintenant des topics "post ou ...", ce qui fait qu'il est beaucoup plus rapide
+// MÀJ 2.0: prend maintenant en compte le sujet (exemple: sujet "cette meuf à la post" avec un message "ou cancer :)" sera détecté comme un topic "post ou ...")
 
 (function() {
 	'use strict';
 	const OUI = 1, oui = 1, NON = 0, non = 0;
-
 
 
 
@@ -44,25 +46,48 @@
 			{
 				let topic_id = doc.querySelector("*[data-topic-id]").getAttribute("data-topic-id");
 				let element = document.querySelector(".conteneur-topic-pagi li[data-id='"+topic_id+"']");
-				if(MASQUER === NON)
-				{
-					element = element.querySelector(".topic-title");
-					element.innerHTML = `<span style="color: red">[POST OU]</span> ` + element.innerHTML.trim();
-				}
-				else
-				{
-					element.remove();
-				}
+				marquer(element);
+				GM_setValue("t"+topicID, sujetPost ? 2 : 1);
+			}
+			else
+			{
+				GM_setValue("t"+topicID, 0);
 			}
 		};
-		GM_xmlhttpRequest({
-			method: "GET",
-			url: el.querySelector(".topic-title").href,
-			onload: callback
-		});
+		let topicID = el.getAttribute("data-id");
+		let cache = GM_getValue("t"+topicID);
+		if(cache === undefined)
+		{
+			GM_xmlhttpRequest({
+				method: "GET",
+				url: el.querySelector(".topic-title").href,
+				onload: callback
+			});
+		}
+		else
+		{
+			if(cache == 1 || cache == 2)
+			{
+				marquer(el);
+			}
+		}
 	});
+
 	function stringToHtml(s)
 	{
 		return (new DOMParser()).parseFromString(s, "text/html");
+	}
+
+	function marquer(element)
+	{
+		if(MASQUER === NON)
+		{
+			element = element.querySelector(".topic-title");
+			element.innerHTML = `<span style="color: red">[POST OU]</span> ` + element.innerHTML.trim();
+		}
+		else
+		{
+			element.remove();
+		}
 	}
 })();
