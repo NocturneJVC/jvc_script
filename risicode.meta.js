830768340560439773ec5873f007b293c6d40408
NocturneJVC <jedisbonjourbordel@gmail.com>
2018-09-14 17:18:41 +0200

    Update risicode.meta.js

--- risicode.meta.js
+++ risicode.meta.js
@@ -1,1 +1,17 @@
-
+// ==UserScript==
+// @name         RisiCode
+// @version      1.0
+// @description  Utilisez vos stickers plus rapidement !
+// @author       NocturneX
+// @match        http://www.jeuxvideo.com/forums/*
+// @icon         http://image.noelshack.com/fichiers/2017/48/7/1512337389-renardban17.png
+// @downloadURL  https://github.com/NocturneJVC/jvc_script/raw/master/risicode.user.js
+// @updateURL    https://github.com/NocturneJVC/jvc_script/raw/master/risicode.meta.js
+// @grant        GM_setValue
+// @grant        GM.setValue
+// @grant        GM_getValue
+// @grant        GM.getValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM.xmlhttpRequest
+// @connect      risibank.fr
+// ==/UserScript==
