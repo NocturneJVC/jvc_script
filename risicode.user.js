b7743e00180c923f6a81b7c1e38541a6f2f974bb
NocturneJVC <jedisbonjourbordel@gmail.com>
2020-09-21 00:01:11 +0200

    Update risicode.user.js
    v1.3
    Première MÀJ depuis 2 ans

--- risicode.user.js
+++ risicode.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         RisiCode
-// @version      1.2
+// @version      1.3
 // @description  Utilisez vos stickers plus rapidement !
 // @author       NocturneX
 // @match        http://www.jeuxvideo.com/forums/*
-// @icon         http://image.noelshack.com/fichiers/2018/48/5/1543610393-logo.png
-// @downloadURL  https://github.com/NocturneJVC/jvc_script/raw/master/risicode.user.js
-// @updateURL    https://github.com/NocturneJVC/jvc_script/raw/master/risicode.meta.js
+// @match        https://www.jeuxvideo.com/forums/*
+// @icon         https://image.jeuxvideo.com/stickers/p/1klb
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
@@ -14,8 +13,7 @@
 // ==/UserScript==
 
 // Le script n'est pas compatible avec Greasemonkey. Utilisez Tampermonkey pour installer vos scripts.
-// Pour effectuer les recherches de sticker, RisiCode utilise le site risibank.fr .
-// Pour accélérer l'affiche des stickers de la partie "Recherche Risibank", RisiCode utilise les images du site jvsticker.com de Alexandre.
+// Pour effectuer les recherches de sticker, RisiCode utilise le site risibank.fr.
 
 new class RisiCode {
 
@@ -114,12 +112,14 @@ new class RisiCode {
             [":masque:", "[[sticker:p/1kko]]"], [":malin:", "[[sticker:p/1jnd]]"], [":hein:", "[[sticker:p/1jne]]"], [":hahaha:", "[[sticker:p/1jnc]]"], [":billets:", "[[sticker:p/1jnj]]"], [":furie:", "[[sticker:p/1jng]]"], [":cigare:", "[[sticker:p/1jnf]]"], [":dehors3:", "[[sticker:p/1jni]]"], [":perplexe:", "[[sticker:p/1jnh]]"], [":dark:", "[[sticker:p/1kgx]]"], [":laser:", "[[sticker:p/1kh1]]"], [":christ:", "[[sticker:p/1kgz]]"],
             [":racaille:", "[[sticker:p/1kgv]]"], [":btg3:", "[[sticker:p/1kgw]]"], [":couronne:", "[[sticker:p/1kgy]]"], [":flamme:", "[[sticker:p/1kgu]]"], [":pls3:", "[[sticker:p/1kh0]]"], [":fete2:", "[[sticker:p/1kl6]]"], [":coeur2:", "[[sticker:p/1kl8]]"], [":coeur3:", "[[sticker:p/1klb]]"], [":koi:", "[[sticker:p/1kl1]]"], [":joue:", "[[sticker:p/1kl9]]"], [":cadeau:", "[[sticker:p/1kl7]]"], [":sleep3:", "[[sticker:p/1kl5]]"],
             [":heureux:", "[[sticker:p/1kl2]]"], [":perplexe3:", "[[sticker:p/1kl3]]"], [":fait chier:", "[[sticker:p/1kky]]"], [":dur2:", "[[sticker:p/1kkz]]"], [":tombe:", "[[sticker:p/1kla]]"], [":queue:", "[[sticker:p/1kl4]]"], [":nn:", "[[sticker:p/1kl0]]"], [":chevalier:", "[[sticker:p/1ljl]]"], [":hop:", "[[sticker:p/1ljj]]"], [":bide:", "[[sticker:p/1ljm]]"],
-            [":photo:", "[[sticker:p/1ljn]]"], [":bescherelle:", "[[sticker:p/1ljo]]"], [":+1:", "[[sticker:p/1ljp]]"], [":master:", "[[sticker:p/1ljr]]"], [":thug:", "[[sticker:p/1ljq]]"], [":go mp:", "[[sticker:p/1rzs]]"], [":troll detector:", "[[sticker:p/1rzt]]"], [":first:", "[[sticker:p/1rzu]]"], [":jvc:", "[[sticker:p/1rzv]]"], [":tant pis:", "[[sticker:p/1rzw]]"], [":fort:", "[[sticker:p/1lgd]]"],
+            [":photo:", "[[sticker:p/1ljn]]"], [":bescherelle:", "[[sticker:p/1ljo]]"], [":+1:", "[[sticker:p/1ljp]]"], [":master:", "[[sticker:p/1ljr]]"], [":thug:", "[[sticker:p/1ljq]]"], [":go mp:", "[[sticker:p/1rzs]]"], [":tant pis:", "[[sticker:p/1rzw]]"], [":fort:", "[[sticker:p/1lgd]]"],
             [":question2:", "[[sticker:p/1lgc]]"], [":combat:", "[[sticker:p/1lgf]]"], [":grogne:", "[[sticker:p/1lgb]]"], [":grukk:", "[[sticker:p/1lgh]]"], [":perplexe2:", "[[sticker:p/1lgg]]"], [":trance:", "[[sticker:p/1lga]]"], [":pouce:", "[[sticker:p/1lge]]"], [":oklm:", "[[sticker:p/zu6]]"], [":oklm2:", "[[sticker:p/1f8e]]"], [":poker:", "[[sticker:p/1f89]]"], [":bud+1:", "[[sticker:p/zuc]]"],
             [":hahah:", "[[sticker:p/zu2]]"], [":dur:", "[[sticker:p/1f8a]]"], [":hin:", "[[sticker:p/zub]]"], [":zzz:", "[[sticker:p/zua]]"], [":grr:", "[[sticker:p/zu9]]"], [":oh:", "[[sticker:p/zu8]]"], [":argh:", "[[sticker:p/1f8d]]"], [":5min:", "[[sticker:p/1f8b]]"],
-            [":bbq:", "[[sticker:p/1f8f]]"], [":sup:", "[[sticker:p/1f88]]"], [":burp:", "[[sticker:p/zu7]]"], [":colis:", "[[sticker:p/1f8c]]"], [":sombrero:", "[[sticker:p/1lmi]]"], [":sleep:", "[[sticker:p/1lml]]"], [":cute2:", "[[sticker:p/1lmh]]"], [":pote:", "[[sticker:p/1lmj]]"], [":triste:", "[[sticker:p/1lmk]]"], [":hotte:", "[[sticker:p/1lmm]]"], [":hey:", "[[sticker:p/1lmn]]"], [":titeuf:", "[[sticker:p/1lmo]]"], [":wc:", "[[sticker:p/1lmp]]"],
+            [":bbq:", "[[sticker:p/1f8f]]"], [":sup:", "[[sticker:p/1f88]]"], [":burp:", "[[sticker:p/zu7]]"], [":sombrero:", "[[sticker:p/1lmi]]"], [":sleep:", "[[sticker:p/1lml]]"], [":cute2:", "[[sticker:p/1lmh]]"], [":pote:", "[[sticker:p/1lmj]]"], [":triste:", "[[sticker:p/1lmk]]"], [":hotte:", "[[sticker:p/1lmm]]"], [":hey:", "[[sticker:p/1lmn]]"], [":titeuf:", "[[sticker:p/1lmo]]"], [":wc:", "[[sticker:p/1lmp]]"],
             [":magic:", "[[sticker:p/1mqv]]"], [":popcorn:", "[[sticker:p/1mqw]]"], [":hey2:", "[[sticker:p/1mqx]]"], [":feed:", "[[sticker:p/1mqy]]"], [":muscu:", "[[sticker:p/1mqz]]"], [":famoso:", "[[sticker:p/1mr0]]"], [":kebab:", "[[sticker:p/1mr1]]"], [":feel:", "[[sticker:p/1nua]]"], [":dehors2:", "[[sticker:p/1nu6]]"], [":papy:", "[[sticker:p/1nu9]]"],
-            [":barbe:", "[[sticker:p/1nu8]]"], [":!:", "[[sticker:p/1nub]]"], [":fleurs:", "[[sticker:p/1nu7]]"], [":boom:", "[[sticker:p/1lmd]]"], [":hahaha3:", "[[sticker:p/1lmb]]"], [":ok2:", "[[sticker:p/1lm9]]"], [":burger:", "[[sticker:p/1lmf]]"], [":deprime:", "[[sticker:p/1lmc]]"], [":regard:", "[[sticker:p/1lme]]"], [":chasse:", "[[sticker:p/1lmg]]"], [":attaque:", "[[sticker:p/1lma]]"]
+            [":barbe:", "[[sticker:p/1nu8]]"], [":!:", "[[sticker:p/1nub]]"], [":fleurs:", "[[sticker:p/1nu7]]"], [":boom:", "[[sticker:p/1lmd]]"], [":hahaha3:", "[[sticker:p/1lmb]]"], [":ok2:", "[[sticker:p/1lm9]]"], [":burger:", "[[sticker:p/1lmf]]"], [":deprime:", "[[sticker:p/1lmc]]"], [":regard:", "[[sticker:p/1lme]]"], [":chasse:", "[[sticker:p/1lmg]]"], [":attaque:", "[[sticker:p/1lma]]"],
+            [":like a boss:", "[[sticker:p/1lt9]]"], [":salut2:", "[[sticker:p/1jc3-fr]]"], [":gg:", "[[sticker:p/1li5]]"], [":star:", "[[sticker:p/1li4]]"],
+            [":allez:", "[[sticker:p/1n1m]]"], [":but:", "[[sticker:p/1n1n]]"], [":deprime2:", "[[sticker:p/1n1o]]"], [":carton rouge:", "[[sticker:p/1n1s]]"], [":victoire:", "[[sticker:p/1n1t]]"], [":what:", "[[sticker:p/1n1q]]"]
         ];
     }
 
@@ -251,7 +251,7 @@ new class RisiCode {
                 liste.push({
                     id: sticker.id,
                     image_noelshack: "http://image.noelshack.com/fichiers/" + sticker.link,
-                    image_jvsticker: "https://i.jvsticker.com/" + sticker.id,
+                    image_risibank: "https://risibank.fr/cache/stickers/d" + (Math.floor(sticker.id/100)) + "/" + sticker.id + "-thumb." + sticker.ext,
                     tags: sticker.tags
                 });
             });
@@ -380,14 +380,14 @@ new class RisiCode {
     }
 
     htmlStickersWebedia () {
-        let html = `<div style="margin-bottom: 5px; border-bottom: 1px solid; padding-bottom: 5px; font-size: 1.2em;">Stickers JVC <a style="float: right; cursor: pointer" data-str-1="[Afficher]" data-str-2="[Masquer]" id="toggle-stkswebedia">[Afficher]</a></div>`;
+        let html = `<div style="margin-bottom: 5px; border-bottom: 1px solid; padding-bottom: 5px; font-size: 1.2em;">Stickers Webedia <a style="float: right; cursor: pointer" data-str-1="[Afficher]" data-str-2="[Masquer]" id="toggle-stkswebedia">[Afficher]</a></div>`;
         html    += `<div id="stkswebedia" style="cursor: auto; display: none; flex-wrap: wrap; justify-content: space-between;">`;
         for(let sticker of this.stickers)
         {
             let afficher;
-            if(/\[\[sticker:([0-9a-zA-Z\/]+)\]\]/.test(sticker[1]))
+            if(/\[\[sticker:([0-9a-zA-Z-\/]+)\]\]/.test(sticker[1]))
             {
-                afficher = '<img src="http://jv.stkr.fr/'+RegExp.$1+'?f-ed=1" height="44px;">';
+                afficher = '<img src="http://image.jeuxvideo.com/stickers/'+RegExp.$1+'" height="44px;">';
             }
             else
             {
@@ -466,14 +466,14 @@ new class RisiCode {
         let afficher;
         if(/^http(s)?:\/\/image.noelshack.com\/(fichiers|minis)\/([0-9]+)\/([0-9]+)\/([0-9]+)-([0-9a-zA-Z-]+).(png|jpg|jpeg|gif)$/.test(perso[1]) ||
            /^http(s)?:\/\/image.noelshack.com\/(fichiers|minis)\/([0-9]+)\/([0-9]+)\/([0-9]+)\/([0-9]+)-([0-9a-zA-Z-]+).(png|jpg|jpeg|gif)$/.test(perso[1])) {
-            afficher = '<img src="'+perso[1]+'" width="68" height="51" class="risicode-bt-st" data-stk="'+perso[1]+'" style="cursor:pointer">';
+            afficher = '<img data-real-src="'+perso[1]+'" width="68" height="51" class="risicode-bt-st" data-stk="'+perso[1]+'" style="cursor:pointer">';
         }
         else if(/^http(s)?:\/\/www\.noelshack\.com\/([0-9]{4})-([0-9]{2})-([0-9]+)-([0-9a-z-]+)\.(png|gif|jpg|jpeg)$/.test(perso[1])) {
             let link = `http://image.noelshack.com/fichiers/${RegExp.$2}/${RegExp.$3}/${RegExp.$4}-${RegExp.$5}.${RegExp.$6}`;
-            afficher = '<img src="'+link+'" width="68" height="51" class="risicode-bt-st" data-stk="'+link+'" style="cursor:pointer">';
+            afficher = '<img data-real-src="'+link+'" width="68" height="51" class="risicode-bt-st" data-stk="'+link+'" style="cursor:pointer">';
         }
         else if(/^\[\[sticker:([0-9a-zA-Z\/]+)\]\]$/.test(perso[1])) {
-            afficher = '<img src="http://jv.stkr.fr/'+RegExp.$1+'?f-ed=1" height="44px;">';
+            afficher = '<img src="image.jeuxvideo.com/stickers/'+RegExp.$1+'" height="44px;">';
         }
         else {
             afficher = '<textarea readonly style="width: 68px;resize: none;">' + perso[1] + '</textarea>';
@@ -515,6 +515,22 @@ new class RisiCode {
             modal("fermer");
             this.modalGerer();
         };
+
+        this.afficheStickersSync()
+    }
+
+    afficheStickersSync () {
+        let img = document.querySelector("*[data-real-src]")
+        if(img) {
+            let src = img.getAttribute("data-real-src")
+            if(/^https?:\/\/image\.noelshack\.com\/fichiers\/(.+)\./.test(src)) {
+                src = `http://image.noelshack.com\/minis\/${RegExp.$1}.png`
+            }
+            img.onload = () => this.afficheStickersSync()
+            img.onerror = () => {img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"}
+            img.src = src
+            img.removeAttribute("data-real-src")
+        }
     }
 
     htmlGererListe () {
@@ -539,6 +555,7 @@ new class RisiCode {
                 modal("centrer");
             };
         }
+        this.afficheStickersSync()
     }
 
     htmlStickersRisibank () {
@@ -604,7 +621,7 @@ new class RisiCode {
                                         html += this.htmlDivStickerRisibank({
                                             id: sticker.id,
                                             image_noelshack: "http://image.noelshack.com/fichiers/" + sticker.link,
-                                            image_jvsticker: "https://i.jvsticker.com/" + sticker.id,
+                                            image_risibank: "https://risibank.fr/cache/stickers/d" + (Math.floor(sticker.id/100)) + "/" + sticker.id + "-thumb." + sticker.ext,
                                             tags: sticker.tags
                                         });
                                     }
@@ -627,7 +644,7 @@ new class RisiCode {
 
     htmlDivStickerRisibank (sticker) {
         return `<div id="risibank-sticker-${sticker.id}" style="margin: 5px;  display: inline-block; min-width: 6%; padding:  5px;border: 1px dashed;border-radius:  3px; cursor: pointer" class="risicode-bt-risibank" data-stk="${sticker.image_noelshack}">
-                          <div style="width: 100%; display: block; text-align: center; margin: 0"><img onerror="this.onerror = function () {this.parentNode.parentNode.remove()};this.src='${sticker.image_noelshack}';" src="${sticker.image_jvsticker}" width="64px" title="${sticker.tags}"></div>
+                          <div style="width: 100%; display: block; text-align: center; margin: 0"><img onerror="this.onerror = function () {this.parentNode.parentNode.remove()};this.src='${sticker.image_noelshack}';" src="${sticker.image_risibank}" width="64px" title="${sticker.tags}"></div>
                           <div style="width: 100%; display: block; text-align: center; margin: 0">:rid ${sticker.id}:</div>
                      </div>`;
     }
